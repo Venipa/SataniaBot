99d6e0b43e827a486003473d596e975b38e84fbe
Tromodolo <Cazionchallosette@gmail.com>
2017-03-14 15:33:05 +0100

    Add project files.

---
+++ TromoBot/Modules/Social/SocialModule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace TromoBot.Modules
+{
+    public class SocialModule : ModuleBase<SocketCommandContext>
+    {
+
+        [Command("test")]
+        public async Task Say([Remainder] string echo)
+        {
+
+            await ReplyAsync(echo);
+
+        }
+        
+    }
+}
