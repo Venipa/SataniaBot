44fd88dbe0c5c01fb57807ac350932fba41cb83b
Tromodolo <Cazionchallosette@gmail.com>
2017-03-27 09:07:47 +0200

    Update commands command and add help command

--- SataniaBot/Modules/Help/HelpModule.cs
+++ SataniaBot/Modules/Help/HelpModule.cs
@@ -21,6 +21,7 @@ namespace SataniaBot.Modules.Help
             foreach (var m in Satania._commands._cmds.Modules)
             {
                 EmbedFieldBuilder embedfield = new EmbedFieldBuilder();
+                embedfield.IsInline = true;
                 embedfield.Name = $"**{m.Name}**";
                 embedfield.Value = string.Join(", ", m.Commands.Select(x => x.Aliases.First()));
 
@@ -30,6 +31,45 @@ namespace SataniaBot.Modules.Help
 
             await ReplyAsync("", embed:embed);
 
+        }
+
+        [Command("help")]
+        public async Task Help(string commandname)
+        {
+            EmbedBuilder embed = new EmbedBuilder();
+            embed.Color = new Color(114, 137, 218);
+
+            var commandinfo = Satania._commands._cmds.Commands.Where(s => s.Name == commandname).First();
+
+            //command name
+            EmbedFieldBuilder nameField = new EmbedFieldBuilder();
+            nameField.IsInline = true;
+            nameField.Name = "Name";
+            nameField.Value = "`" + Satania.db.getPrefix(Context.Guild.Id.ToString()) + commandinfo.Name + "`";
+            embed.AddField(nameField);
+
+            //Command Usage
+            EmbedFieldBuilder usageField = new EmbedFieldBuilder();
+            usageField.IsInline = true;
+            usageField.Name = "Usage";
+            usageField.Value = commandinfo.Summary;
+            embed.AddField(usageField);
+
+            //Command Parameters
+            EmbedFieldBuilder parameterField = new EmbedFieldBuilder();
+            parameterField.IsInline = true;
+            parameterField.Name = "Parameters";
+            parameterField.Value = String.Join(", ", commandinfo.Parameters.Select(x => x.Name));
+            embed.AddField(parameterField);
+
+            //Command Example
+            EmbedFieldBuilder exampleField = new EmbedFieldBuilder();
+            exampleField.IsInline = true;
+            exampleField.Name = "Example";
+            exampleField.Value = "`" + Satania.db.getPrefix(Context.Guild.Id.ToString()) + commandinfo.Remarks + "`";
+            embed.AddField(exampleField);
+
+            await ReplyAsync("", embed:embed);
         }
     }
 }
