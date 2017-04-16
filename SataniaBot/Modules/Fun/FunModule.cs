42be03df17a5d196ae6a61d598363a8f5a65fdae
Tromodolo <Cazionchallosette@gmail.com>
2017-04-16 23:47:33 +0200

    Add Fun module

---
+++ SataniaBot/Modules/Fun/FunModule.cs
@@ -0,0 +1,17 @@
+﻿using Discord.Addons.InteractiveCommands;
+using Discord.Commands;
+using Discord;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SataniaBot.Modules
+{
+    [Name("Social")]
+    public class FunModule : InteractiveModuleBase<SocketCommandContext>
+    {
+
+
+    }
+}
+
