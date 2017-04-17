76722933f890d4b5412125a2c5ed5d7a1414f398
Tromodolo <Cazionchallosette@gmail.com>
2017-04-17 23:15:31 +0200

    Add NSFWModule

---
+++ SataniaBot/Modules/NSFW/NSFWModule.cs
@@ -0,0 +1,20 @@
+﻿using Discord.Addons.InteractiveCommands;
+using Discord.Commands;
+using Discord;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using SataniaBot.Services.EmbedExtensions;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SataniaBot.Modules
+{
+    [Name("NSFW")]
+    public class NSFWModule : InteractiveModuleBase<SocketCommandContext>
+    {
+
+
+    }
+}
+
