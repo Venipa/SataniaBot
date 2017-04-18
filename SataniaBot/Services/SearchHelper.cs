c0647eed8346a22715c6f868e354d3c82b55616f
Tromodolo <Cazionchallosette@gmail.com>
2017-04-18 02:17:20 +0200

    Copy code from nadekobot for nsfw that actually works

--- SataniaBot/Services/SearchHelper.cs
+++ SataniaBot/Services/SearchHelper.cs
@@ -11,6 +11,7 @@ using System.Xml.Linq;
 using Google.Apis.Services;
 using SataniaBot;
 using SataniaBot.Services;
+using System.Xml;
 
 namespace Satania.Services
 {
@@ -23,137 +24,98 @@ namespace Satania.Services
 
     public static class SearchHelper
     {
+        public enum WebsiteType
+        {
+            Safebooru,
+            Gelbooru,
+            Rule34,
+            Konachan,
+            Yandere
+        }
+
         private static DateTime lastRefreshed = DateTime.MinValue;
         private static string token { get; set; } = "";
         private static readonly HttpClient httpClient = new HttpClient();
         private static Random rng = new Random();
 
-
-        public static async Task<Stream> GetResponseStreamAsync(string url,
-            IEnumerable<KeyValuePair<string, string>> headers = null, RequestHttpMethod method = RequestHttpMethod.Get)
+        public static async Task<string> PictureSearch(string tag, WebsiteType type)
         {
-            if (string.IsNullOrWhiteSpace(url))
-                throw new ArgumentNullException(nameof(url));
-            //if its a post or there are no headers, use static httpclient
-            // if there are headers and it's get, it's not threadsafe
-            var cl = headers == null || method == RequestHttpMethod.Post ? httpClient : new HttpClient();
-            cl.DefaultRequestHeaders.Clear();
-            switch (method)
+            tag = tag?.Replace(" ", "_");
+            var website = "";
+            switch (type)
             {
-                case RequestHttpMethod.Get:
-                    if (headers != null)
+                case WebsiteType.Safebooru:
+                    website = $"https://safebooru.org/index.php?page=dapi&s=post&q=index&limit=100&tags={tag}";
+                    break;
+                case WebsiteType.Gelbooru:
+                    website = $"http://gelbooru.com/index.php?page=dapi&s=post&q=index&limit=100&tags={tag}";
+                    break;
+                case WebsiteType.Rule34:
+                    website = $"https://rule34.xxx/index.php?page=dapi&s=post&q=index&limit=100&tags={tag}";
+                    break;
+                case WebsiteType.Konachan:
+                    website = $"https://konachan.com/post.xml?s=post&q=index&limit=100&tags={tag}";
+                    break;
+                case WebsiteType.Yandere:
+                    website = $"https://yande.re/post.xml?limit=100&tags={tag}";
+                    break;
+            }
+            try
+            {
+                var toReturn = await Task.Run(async () =>
+                {
+                    using (var http = new HttpClient())
                     {
-                        foreach (var header in headers)
-                        {
-                            cl.DefaultRequestHeaders.TryAddWithoutValidation(header.Key, header.Value);
-                        }
+                        http.AddFakeHeaders();
+                        var data = await http.GetStreamAsync(website).ConfigureAwait(false);
+                        var doc = new XmlDocument();
+                        doc.Load(data);
+
+                        var node = doc.LastChild.ChildNodes[rng.Next(0, doc.LastChild.ChildNodes.Count)];
+
+                        var url = node.Attributes["file_url"].Value;
+                        if (!url.StartsWith("http"))
+                            url = "https:" + url;
+                        return url;
                     }
-                    return await cl.GetStreamAsync(url).ConfigureAwait(false);
-                case RequestHttpMethod.Post:
-                    FormUrlEncodedContent formContent = null;
-                    if (headers != null)
-                    {
-                        formContent = new FormUrlEncodedContent(headers);
-                    }
-                    var message = await cl.PostAsync(url, formContent).ConfigureAwait(false);
-                    return await message.Content.ReadAsStreamAsync().ConfigureAwait(false);
-                default:
-                    throw new NotImplementedException("That type of request is unsupported.");
+                }).ConfigureAwait(false);
+                return toReturn;
+            }
+            catch
+            {
+                return null;
             }
         }
 
-        public static async Task<string> GetResponseStringAsync(string url,
-            IEnumerable<KeyValuePair<string, string>> headers = null,
-            RequestHttpMethod method = RequestHttpMethod.Get)
-        {
-
-            using (var streamReader = new StreamReader(await GetResponseStreamAsync(url, headers, method).ConfigureAwait(false)))
-            {
-                return await streamReader.ReadToEndAsync().ConfigureAwait(false);
-            }
-        }
-
-        public static async Task<string> GetDanbooruImageLink(string tag)
-        {
-
-            if (tag == "loli") //loli doesn't work for some reason atm
-                tag = "flat_chest";
-
-            var link = $"http://danbooru.donmai.us/posts?" +
-                        $"page={rng.Next(0, 15)}";
-            if (!string.IsNullOrWhiteSpace(tag))
-                link += $"&tags={tag.Replace(" ", "_")}";
-
-            var webpage = await GetResponseStringAsync(link).ConfigureAwait(false);
-            var matches = Regex.Matches(webpage, "data-large-file-url=\"(?<id>.*?)\"");
-
-            if (matches.Count == 0)
-                return null;
-            return $"http://danbooru.donmai.us" +
-                   $"{matches[rng.Next(0, matches.Count)].Groups["id"].Value}";
-        }
-
-        public static async Task<string> GetGelbooruImageLink(string tag)
-        {
-            var headers = new Dictionary<string, string>() {
-                {"User-Agent", "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/14.0.835.202 Safari/535.1"},
-                {"Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8" },
-            };
-            var url =
-                $"http://gelbooru.com/index.php?page=dapi&s=post&q=index&limit=100&tags={tag.Replace(" ", "_")}";
-            var webpage = await GetResponseStringAsync(url, headers).ConfigureAwait(false);
-            var matches = Regex.Matches(webpage, "file_url=\"(?<url>.*?)\"");
-            if (matches.Count == 0)
-                return null;
-            var match = matches[rng.Next(0, matches.Count)];
-            return matches[rng.Next(0, matches.Count)].Groups["url"].Value;
-        }
-
-        public static async Task<string> GetSafebooruImageLink(string tag)
-        {
-            var url =
-            $"http://safebooru.org/index.php?page=dapi&s=post&q=index&limit=100&tags={tag.Replace(" ", "_")}";
-            var webpage = await GetResponseStringAsync(url).ConfigureAwait(false);
-            var matches = Regex.Matches(webpage, "file_url=\"(?<url>.*?)\"");
-            if (matches.Count == 0)
-                return null;
-            var match = matches[rng.Next(0, matches.Count)];
-            return matches[rng.Next(0, matches.Count)].Groups["url"].Value;
-        }
-
-        public static async Task<string> GetRule34ImageLink(string tag)
-        {
-            var url =
-            $"http://rule34.xxx/index.php?page=dapi&s=post&q=index&limit=100&tags={tag.Replace(" ", "_")}";
-            var webpage = await GetResponseStringAsync(url).ConfigureAwait(false);
-            var matches = Regex.Matches(webpage, "file_url=\"(?<url>.*?)\"");
-            if (matches.Count == 0)
-                return null;
-            var match = matches[rng.Next(0, matches.Count)];
-            return "http:" + matches[rng.Next(0, matches.Count)].Groups["url"].Value;
-        }
-
-
-        internal static async Task<string> GetE621ImageLink(string tags)
+        public static Task<string> GetE621ImageLink(string tag) => Task.Run(async () =>
         {
             try
             {
-                var headers = new Dictionary<string, string>() {
-                    {"User-Agent", "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/14.0.835.202 Safari/535.1"},
-                    {"Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8" },
-                };
-                var data = await GetResponseStreamAsync(
-                    "http://e621.net/post/index.xml?tags=" + Uri.EscapeUriString(tags) + "%20order:random&limit=1",
-                    headers);
-                var doc = XDocument.Load(data);
-                return doc.Descendants("file_url").FirstOrDefault().Value;
+                using (var http = new HttpClient())
+                {
+                    http.AddFakeHeaders();
+                    var data = await http.GetStreamAsync("http://e621.net/post/index.xml?tags=" + tag).ConfigureAwait(false);
+                    var doc = new XmlDocument();
+                    doc.Load(data);
+                    var nodes = doc.GetElementsByTagName("file_url");
+
+                    var node = nodes[rng.Next(0, nodes.Count)];
+                    return node.InnerText;
+                }
             }
-            catch (Exception ex)
+            catch
             {
-                Console.WriteLine("Error in e621 search: \n" + ex);
-                return "Error, do you have too many tags?";
+                return null;
             }
+        });
+
+        public static void AddFakeHeaders(this HttpClient http)
+        {
+            http.DefaultRequestHeaders.Clear();
+            http.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/14.0.835.202 Safari/535.1");
+            http.DefaultRequestHeaders.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
         }
+
         /*
         public static string GoogleSearch(string q)
         {
