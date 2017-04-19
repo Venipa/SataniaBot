018f4e9d9ca3935f89097e901bb5edd368cc80f0
F.Angels <Tom.Sid@wp.pl>
2017-04-19 23:43:39 +0000

    New Module created and .coinflip added

---
+++ SataniaBot/Modules/Fun/GameCommands.cs
@@ -0,0 +1,39 @@
+﻿using Discord.Addons.InteractiveCommands;
+using Discord.Commands;
+using Discord;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using SataniaBot.Services.EmbedExtensions;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SataniaBot.Modules
+{
+    [Name("Games")]
+    public class GameModules : InteractiveModuleBase<SocketCommandContext>
+    {
+        [Command("CoinFlip")]
+        public async Task Flip()
+        {
+
+            Random flip = new Random();
+            var coin = (flip.Next(1, 100));
+            string CoinSide = "";
+            if (coin <= 50)
+            {
+                CoinSide = "Heads";
+            }
+            else if (coin >= 51)
+            {
+                CoinSide = "Tails";
+            }
+
+            await Context.Channel.SendConfirmAsync(CoinSide);
+
+        }
+
+        
+    }
+}
+
