449413e0ee90f4511245bdf8f243d048f95e0f0b
Tromodolo <Cazionchallosette@gmail.com>
2017-05-28 15:41:50 +0200

    Create Utility Module

---
+++ SataniaBot/Modules/Utility/UtilityModule.cs
@@ -0,0 +1,20 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using SataniaBot.Services.EmbedExtensions;
+
+namespace SataniaBot.Modules
+{
+    [Name("Utility")]
+    public class UtilityModule : ModuleBase<SocketCommandContext>
+    {
+
+    }
+}
